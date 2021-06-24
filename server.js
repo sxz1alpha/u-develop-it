2c1d53a03204e2a2b9ee4435ef6a60e34934143c
UCDT <adrian.j.ulibarri@gmail.com>
2021-06-24 13:12:17 -0600

    added dotenv

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mysql = require('mysql2');
+const db = require('db');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+db.connect(
+    {
+        host: process.env.DB_HOST,
+        // Your MySQL username,
+        user: process.env.DB_USER,
+        //Your MySQL password
+        password: process.env.DB_PASS,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
