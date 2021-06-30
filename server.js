de048e68b7ff06fd6be19153d6751ebf983e6b34
UCDT <adrian.j.ulibarri@gmail.com>
2021-06-30 09:52:41 -0600

    added parties table and wrote JOIN LEFT statments for get requests

--- server.js
+++ server.js
@@ -76,7 +76,12 @@ app.post('/api/candidate', ({ body }, res) => {
 
 //GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates. *, party.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -92,7 +97,11 @@ app.get('/api/candidate/:id', (req, res) => {
 });
 // get all the candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates. *, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
